dcd36ff878f857d9108a45d6f6451b290c7c2ead
rafaelcarlosoficial <143357132+rafaelcarlosoficial@users.noreply.github.com>
2024-01-11 17:36:06 -0300

    Teste
    Caso de algum problema na otmização dos botões, retorne aqui.

--- calculator/src/components/ButtonsContainer/ButtonsContainer.jsx
+++ calculator/src/components/ButtonsContainer/ButtonsContainer.jsx
@@ -1,64 +1,151 @@
-import React, {useContext} from 'react'
-import Button from '../Button/Button'
+// import React, {useContext} from 'react'
+// import Button from '../Button/Button'
+// import './ButtonsContainer.css';
+// import AppContext from '../../context/context';
+
+// const ButtonsContainer = () => {
+//     const contentOfTheButtons = 
+//     ['%', 'CE', 'C', '⌫', '1/X', 
+//     'X²', '²√x', '÷', '7', '8', 
+//     '9','X', '4', '5', '6', '-', 
+//     '1', '2', '3', '+', '+/-', 
+//     '0', '.', '=']
+
+//     const grayButtons = 
+//     ['7', '8', '9', 
+//     '4', '5', '6', 
+//     '1', '2', '3', 
+//     '+/-', '0', '.' ]
+
+//     const numberButtons = 
+//     ['1', '2', '3', 
+//     '4', '5', '6', 
+//     '7', '8', '9', 
+//     '0', '.'];
+
+//     const operators = [
+//       'X', '÷', '+', '-', 
+//     ]
+
+//     const deleteNumbers = ['C', '⌫']
+     
+//     const secondayOperator = ['²√x', 'X²', '1/X', '%', '+/-', 'CE']
+
+//     const { insertNumber, insertOperator, calculate, secondaryCalculations, deleteFromScreen } = useContext(AppContext);
+//     return (
+//     <div className='buttonsContainer'>
+//         {contentOfTheButtons.map((content, index) => {
+//            //fazer um switch e deixar isso mais organizado
+//            if(grayButtons.includes(content)  && content !== '+/-'){
+              
+//              if(grayButtons.includes(content) && numberButtons.includes(content)){
+//                 return (<Button key={index} content={content} background={'#3B3B3B'} handleClick={insertNumber} />);
+//               } else {
+//                 return (<Button key={index} content={content} background={'#3B3B3B'} />);
+//               }
+
+//            } 
+           
+//            else if(content === '=') {
+            
+//             return (<Button key={index} content={content} background={'#76B9ED'} textColor={'#202020'} handleClick={calculate} />);
+           
+//            } else if (operators.includes(content)){
+            
+//             return (<Button key={index} content={content} handleClick={insertOperator} />);
+
+//            } else if (secondayOperator.includes(content)) {
+
+//             return (<Button key={index} content={content} handleClick={secondaryCalculations}/>)
+//            }
+
+//            else if (deleteNumbers.includes(content)) {
+//             return (<Button key={index} content={content} handleClick={deleteFromScreen} />);
+//            }
+          
+//         })}
+  
+//     </div>
+//   )
+// }
+
+
+// export default ButtonsContainer
+
+
+import React, { useContext, useMemo, useCallback } from 'react';
+import Button from '../Button/Button';
 import './ButtonsContainer.css';
 import AppContext from '../../context/context';
 
 const ButtonsContainer = () => {
-    const contentOfTheButtons = 
-    ['%', 'CE', 'C', '⌫', '1/X', 
-    'X²', '²√x', '÷', '7', '8', 
-    '9','X', '4', '5', '6', '-', 
-    '1', '2', '3', '+', '+/-', 
-    '0', '.', '=']
+  const contentOfTheButtons = useMemo(() => [
+    '%', 'CE', 'C', '⌫', '1/X',
+    'X²', '²√x', '÷', '7', '8',
+    '9', 'X', '4', '5', '6', '-',
+    '1', '2', '3', '+', '+/-',
+    '0', '.', '='
+  ], []);
 
-    const grayButtons = 
-    ['7', '8', '9', 
-    '4', '5', '6', 
-    '1', '2', '3', 
-    '+/-', '0', '.' ]
+  const grayButtons = useMemo(() => [
+    '7', '8', '9',
+    '4', '5', '6',
+    '1', '2', '3',
+    '+/-', '0', '.'
+  ], []);
 
-    const numberButtons = 
-    ['1', '2', '3', 
-    '4', '5', '6', 
-    '7', '8', '9', 
-    '0'];
+  const numberButtons = useMemo(() => [
+    '1', '2', '3',
+    '4', '5', '6',
+    '7', '8', '9',
+    '0', '.'
+  ], []);
 
-    const operators = [
-      'X', '÷', '+', '-', 
-    ]
-     
+  const operators = useMemo(() => [
+    'X', '÷', '+', '-',
+  ], []);
 
-    const { insertNumber, insertOperator, calculate } = useContext(AppContext);
-    return (
+  const deleteNumbers = useMemo(() => ['C', '⌫'], []);
+
+  const secondayOperator = useMemo(() => [
+    '²√x', 'X²', '1/X', '%', '+/-', 'CE'
+  ], []);
+
+  const { insertNumber, insertOperator, calculate, secondaryCalculations, deleteFromScreen } = useContext(AppContext);
+
+  const handleButtonClick = useCallback((content) => {
+    switch (content) {
+      case '=':
+        return <Button key={content} content={content} background={'#76B9ED'} textColor={'#202020'} handleClick={calculate} />;
+      case '+':
+      case '-':
+      case 'X':
+      case '÷':
+        return <Button key={content} content={content} handleClick={insertOperator} />;
+      case '²√x':
+      case 'X²':
+      case '1/X':
+      case '%':
+      case '+/-':
+      case 'CE':
+        return <Button key={content} content={content} handleClick={secondaryCalculations} />;
+      case 'C':
+      case '⌫':
+        return <Button key={content} content={content} handleClick={deleteFromScreen} />;
+      default:
+        if (grayButtons.includes(content) && numberButtons.includes(content)) {
+          return <Button key={content} content={content} background={'#3B3B3B'} handleClick={insertNumber} />;
+        } else {
+          return <Button key={content} content={content} background={'#3B3B3B'} />;
+        }
+    }
+  }, [calculate, deleteFromScreen, grayButtons, insertNumber, insertOperator, numberButtons, secondaryCalculations]);
+
+  return (
     <div className='buttonsContainer'>
-        {contentOfTheButtons.map((content, index) => {
-           //fazer um switch e deixar isso mais organizado
-           if(grayButtons.includes(content)){
-              
-             if(grayButtons.includes(content) && numberButtons.includes(content)){
-                return (<Button key={index} content={content} background={'#3B3B3B'} handleClick={insertNumber} />);
-              } else {
-                return (<Button key={index} content={content} background={'#3B3B3B'} />);
-              }
-
-           } 
-           
-           else if(content === '=') {
-            
-            return (<Button key={index} content={content} background={'#76B9ED'} textColor={'#202020'} handleClick={calculate} />);
-           
-           } else if (operators.includes(content)){
-            
-            return (<Button key={index} content={content} handleClick={insertOperator} />);
-
-           } else {
-            return (<Button key={index} content={content} />);
-           }
-          
-        })}
-  
+      {contentOfTheButtons.map((content) => handleButtonClick(content))}
     </div>
-  )
-}
+  );
+};
 
-export default ButtonsContainer
+export default ButtonsContainer;
--- calculator/src/context/Provider.jsx
+++ calculator/src/context/Provider.jsx
@@ -10,10 +10,73 @@ function Provider({ children }){
     const [previousValue, setPreviousValue] = useState();
 
 
+    const contentOfTheButtons = 
+    ['%', 'CE', 'C', '⌫', '1/X', 
+    'X²', '²√x', '÷', '7', '8', 
+    '9','X', '4', '5', '6', '-', 
+    '1', '2', '3', '+', '+/-', 
+    '0', '.', '=']
+
+    const secondaryCalculations = (e) => {
+        let operator = e.target.innerText;
+        setPreviousValue('')
+        
+        switch(operator){
+            case '²√x':
+                if(Math.sign(number) === -1) return;
+                setNumber((Math.sqrt(number)))
+                break;
+            case 'X²':
+                setNumber((Math.pow(number, 2)))
+                break; 
+            case '1/X':
+                if(number !== '0'){
+                    setNumber(1 / number);
+                }
+                break;
+            case '%':
+                setNumber(number / 100)
+                break;
+            case '+/-':
+                setNumber(number * -1)
+                break;
+            case 'CE':
+                setNumber(0)
+                break;
+            default:
+                console.log('Este operador não foi encontrado. (secondaryOperator)')
+                break;
+        }
+
+
+
+    }
+
+    const deleteFromScreen = (e) => {
+        const button = e.target.innerText;
+
+        switch(button){
+            case 'C':
+                setNumber(number.toString().slice(0, -1))
+                break;
+            case '⌫':
+                setNumber(number.toString().slice(0, -1))
+                break;
+            default:
+                console.log('oi')
+                break;
+        }
+      
+    }
 
 
     const calculate = () => {
+   
+        if((oldNumber === null && oldOperator === null) || number === ''){
+            return;
+        }
 
+        
         switch (oldOperator){
             case '/':
                 setNumber(parseFloat(oldNumber) / parseFloat(number))
@@ -33,17 +96,18 @@ function Provider({ children }){
         } 
 
         setPreviousValue(oldNumber + oldOperator + number)
-        setOldNumber('')
-        setOldOperator('')
+        setOldNumber(null)
+        setOldOperator(null)
 
     }
 
 
     const insertOperator = (e) => {
-    
+        if(number === '') return;
         let currentOperator = e.target.innerText;
 
         if(oldOperator === null && oldNumber === null){
+            setPreviousValue('')
             setOldNumber(number)
             setOldOperator(currentOperator)
             setNumber('')
@@ -56,9 +120,12 @@ function Provider({ children }){
 
     const insertNumber = (e) => { 
 
-        
         let currentNumber = e.target.innerText;
         
+        if(currentNumber === '.' && (number.toString().includes('.') || number === '')){
+            return;
+        }
+
         if(number === 0){
             setNumber(currentNumber)
         } else {
@@ -82,7 +149,9 @@ function Provider({ children }){
         setOldNumber,
         calculate,
         previousValue, 
-        setPreviousValue
+        setPreviousValue,
+        secondaryCalculations,
+        deleteFromScreen
     }
 
 
